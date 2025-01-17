7df2cf89eacdfc6cf818ff109ad2be003e4775e3
samin <samin.shafkat@gmail.com>
2025-01-17 13:17:06 +0600

    added search by location module with prompt

--- mapIt.py
+++ mapIt.py
@@ -1,13 +1,38 @@
-import webbrowser, sys, pyperclip
+import webbrowser, sys, pyperclip, time
 
-if len(sys.argv) > 1:
-    #Get address from the command-line
-    address = ' '.join(sys.argv[1:])
+# function to search location by clipboard or input
+def search_location_from_clipboard_or_input():
+    if len(sys.argv) > 1:
+        #Get address from the command-line
+        address = ' '.join(sys.argv[1:])
+    else:
+        # Get address from the clipboard
+        address = pyperclip.paste()
+    #Get address from clipboard
+    webbrowser.open('https://www.google.com/maps/place/' + address)
+ 
+
+def search_location_from_file():
+    file = open('location.txt', 'r')
+    addresslist = file.readlines()
+    file.close()
+    
+    for address in addresslist:
+        webbrowser.open('https://www.google.com/maps/place/' + address)
+        time.sleep(1)
+
+
+
+# Prompt user to choose method
+choice = input("Choose method to search location:\n"
+               + "1: Clipboard/Input\n"
+               + "2: File\n")
+
+if choice == '1':
+    # search by clipboard or input
+    search_location_from_clipboard_or_input()
+elif choice == '2':
+    # search by file
+    search_location_from_file()
 else:
-    # Get address from the clipboard
-    address = pyperclip.paste()
-
-#TODO: Get address from clipboard
-webbrowser.open('https://www.google.com/maps/place/' + address)
-
-
+    print("Invalid choice. Please enter 1 or 2.")
